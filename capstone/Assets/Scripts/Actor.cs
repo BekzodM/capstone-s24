13719ec65c3231f69aef97aa4a630146ad5d9b90
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2024-02-09 14:04:35 -0500

    Created an Animator Controller for walking and idle animation
    - also installed Cinemachine Camera Package

--- capstone/Assets/Scripts/Actor.cs
+++ capstone/Assets/Scripts/Actor.cs
@@ -7,6 +7,7 @@ public class Actor : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -33,5 +34,15 @@ public class Actor : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 12f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
