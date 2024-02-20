657a550c94ad4036b71cc983b85279a6de376a22
Cindy Chan <cindy.y.chan80@gmail.com>
2024-02-19 20:46:34 -0500

    Offensive Projectile is launched towards the enemy

--- capstone/Assets/Scripts/StructureScripts/StructureTypes/Offensive.cs
+++ capstone/Assets/Scripts/StructureScripts/StructureTypes/Offensive.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public abstract class Offensive : Structure
 {
     [SerializeField] private GameObject attackPrefab;
+    [SerializeField] private float projectileDistanceFromHead = 1.5f;
+    [SerializeField] private float projectileSpeed = 20f;
     protected Offensive(string name, string description, int cost, int health, float areaEffectRadius)
         : base(name, description, "Offensive", cost, health, areaEffectRadius)
     {
     }
 
-    public void Shoot(GameObject target, float projectileDistanceFromHead = 1f) {
+    public void Shoot(GameObject target) {
         GameObject model = transform.GetChild(1).gameObject;
-        GameObject modelHead = model.transform.GetChild(0).gameObject;
+        Transform modelHead = model.transform.GetChild(0);
         Vector3 direction = target.transform.position - modelHead.transform.position;
         if (direction != Vector3.zero)
         {
             //rotate head
-            modelHead.transform.rotation = Quaternion.LookRotation(direction);
+            modelHead.rotation = Quaternion.LookRotation(direction);
+
             //instantiate projectile
-            Vector3 spawnPos = modelHead.transform.position + modelHead.transform.forward * projectileDistanceFromHead;
-            Instantiate(attackPrefab, spawnPos, Quaternion.identity);
+            Vector3 spawnPos = modelHead.position + modelHead.forward * projectileDistanceFromHead;
+            if (attackPrefab != null) {
+                GameObject projectileInstance = Instantiate(attackPrefab, spawnPos, Quaternion.identity);
+                
+                //launching projectile
+                Rigidbody attackRB = projectileInstance.GetComponent<Rigidbody>();
+                Debug.Log(attackRB.name);
+                if (attackRB != null)
+                {
+                    attackRB.velocity = direction.normalized * projectileSpeed;            
+                }
+            }
+
+
         }
     }
 }
