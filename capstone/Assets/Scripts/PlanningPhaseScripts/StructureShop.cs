0e28baa6ea74247d5f8f4f8470e838f8d5a9ab74
Cindy Chan <cindy.y.chan80@gmail.com>
2024-02-26 14:43:29 -0500

    Add basic Planning Phase UI

--- capstone/Assets/Scripts/PlanningPhaseScripts/StructureShop.cs
+++ capstone/Assets/Scripts/PlanningPhaseScripts/StructureShop.cs
@@ -2,10 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StructureShop : MonoBehaviour
 {
-    [SerializeField] private GameObject[] structurePrefabs;
+    [SerializeField] private GameObject[] offensiveStructurePrefabs;
+    [SerializeField] private GameObject[] defensiveStructurePrefabs;
+    [SerializeField] private GameObject[] supportStructurePrefabs;
+    [SerializeField] private GameObject[] trapStructurePrefabs;
+    [SerializeField] private GameObject structureShopPrefab;
+    private GameObject[][] structurePrefabs;
+    private int previousIndex = 0;
+
+    private void Start()
+    {
+        structurePrefabs = new GameObject[][]
+            {
+                offensiveStructurePrefabs,
+                defensiveStructurePrefabs,
+                supportStructurePrefabs,
+                trapStructurePrefabs
+            };
+
+        //Fill shop with buttons
+
+        Transform shopContent = transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0); //Content
+
+        for (int i = 0; i < shopContent.childCount; i++)
+        {
+            Transform child = shopContent.GetChild(i);//Offensive..Structures container
+            GameObject[] prefabs = structurePrefabs[i];
+
+            foreach (GameObject obj in prefabs)
+            {
+                GameObject buttonInstance = Instantiate(structureShopPrefab);
+                Button buttonComponent = buttonInstance.GetComponent<Button>();
+                buttonComponent.onClick.AddListener(HandleButtonClick);
+                buttonInstance.transform.SetParent(child.transform);
+            }
+        }
+    }
+
+    //Structure Type Buttons
+    public void showStructureShop(int index)
+    {
+        Transform shopContent = transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0);
+        GameObject previousShopContent = shopContent.GetChild(previousIndex).gameObject;
+        GameObject child = shopContent.GetChild(index).gameObject;
+
+        previousShopContent.SetActive(false);
+        child.SetActive(true);
+        previousIndex = index;
+    }
+
+    //Structure Shop Button onClick function
+    private void HandleButtonClick() {
+        GameObject structureInfo = transform.GetChild(1).gameObject;
+
+        structureInfo.SetActive(!structureInfo.activeSelf);
+
+    }
+
+
+    /*
     public void InstantiatePrefabOnClick(int prefabIndex)
     {
         if (prefabIndex >= 0 && prefabIndex < structurePrefabs.Length)
@@ -16,4 +75,7 @@ public class StructureShop : MonoBehaviour
             Debug.LogWarning("Invalid prefab index: " + prefabIndex);
         }
     }
+    */
+  
+
 }
