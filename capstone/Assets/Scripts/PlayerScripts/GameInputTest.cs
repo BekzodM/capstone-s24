bc618670ebd8fa7f719504903c95739a721b9620
William Yang <yangw031@gmail.com>
2024-02-28 20:14:22 -0500

    Player class movement refactored, next is animator

---
+++ capstone/Assets/Scripts/PlayerScripts/GameInputTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Refactor
+public class GameInputTest : MonoBehaviour
+{
+    //private ActorInputActions actorInputActions;
+
+    //private void Awake()
+    //{
+    //    actorInputActions = new ActorInputActions();
+    //    actorInputActions.Player.Enable();
+    //}
+    //public Vector2 GetMovementVectorNormalized()
+    //{
+    //    Vector2 inputVector = actorInputActions.Player.Move.ReadValue<Vector2>();
+
+    //    inputVector = inputVector.normalized;
+
+    //    Debug.Log(inputVector);
+
+    //    return inputVector;
+    //}
+}
