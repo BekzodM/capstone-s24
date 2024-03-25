d4528d5b09e9f7060e9caeb50c6403a21adc6ce2
BekzodM <bmusayev07@gmail.com>
2024-03-25 01:19:35 -0400

    Add map and camera.

--- capstone/Assets/Scripts/LevelSelect/LevelSelectPlayer.cs
+++ capstone/Assets/Scripts/LevelSelect/LevelSelectPlayer.cs
@@ -16,7 +16,9 @@ public class LevelSelectPlayer : MonoBehaviour
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * 5f);
+        characterController.Move(move * Time.deltaTime * 25f);
         
+        float rotateSpeed = 12f;
+        transform.forward = Vector3.Slerp(transform.forward, move, Time.deltaTime * rotateSpeed);
     }
 }
