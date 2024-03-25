914cd3b287d071d917e2af237ee543c6e6e8ab3e
BekzodM <bmusayev07@gmail.com>
2024-03-25 04:33:02 -0400

    Create and add a plane 3d model.

--- capstone/Assets/Scripts/LevelSelect/LevelSelectPlayer.cs
+++ capstone/Assets/Scripts/LevelSelect/LevelSelectPlayer.cs
@@ -16,7 +16,7 @@ public class LevelSelectPlayer : MonoBehaviour
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * 25f);
+        characterController.Move(move * Time.deltaTime * 30f);
         
         float rotateSpeed = 12f;
         transform.forward = Vector3.Slerp(transform.forward, move, Time.deltaTime * rotateSpeed);
