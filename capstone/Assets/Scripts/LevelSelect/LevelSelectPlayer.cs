be8998dc5b859906cc074ab5cdfdf6677ea0522c
BekzodM <bmusayev07@gmail.com>
2024-03-27 06:49:06 -0400

    Update lighting

--- capstone/Assets/Scripts/LevelSelect/LevelSelectPlayer.cs
+++ capstone/Assets/Scripts/LevelSelect/LevelSelectPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LevelSelectPlayer : MonoBehaviour
 {
     CharacterController characterController;
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,7 @@ public class LevelSelectPlayer : MonoBehaviour
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * 30f);
+        characterController.Move(move * Time.deltaTime * speed);
         
         float rotateSpeed = 12f;
         transform.forward = Vector3.Slerp(transform.forward, move, Time.deltaTime * rotateSpeed);
