0e6dd5c2ed8a81f2475a0c28e6da100929fe65d6
BekzodM <bmusayev07@gmail.com>
2024-04-03 01:08:47 -0400

    Update Base to take damage and destroy enemy.

--- capstone/Assets/saved scenes/PlayerUI_Canvas/BaseDamage.cs
+++ capstone/Assets/saved scenes/PlayerUI_Canvas/BaseDamage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BaseDamage : MonoBehaviour
 {
-    public float radius = 0.2f;
+    public float radius = 0.5f;
     
     public Transform basePoint;
     public GameObject[] allEnemies;
