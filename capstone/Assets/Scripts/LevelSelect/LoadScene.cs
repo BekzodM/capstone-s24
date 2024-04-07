25ec8dca688b9ffec7beee44ca7b3fadeef12278
BekzodM <bmusayev07@gmail.com>
2024-04-07 19:51:27 -0400

    Deactivate padlock image for unlocked levels.

--- capstone/Assets/Scripts/LevelSelect/LoadScene.cs
+++ capstone/Assets/Scripts/LevelSelect/LoadScene.cs
@@ -5,7 +5,13 @@ using System;
 public class LoadScene : MonoBehaviour
 {
     [SerializeField] string SceneName;
-    [SerializeField] int level;
+    [SerializeField] int level; 
+    public void Start() {
+        Transform padlock = transform.Find("Locked");
+        if (GameState.currentProgressLevel >= level) {
+            padlock.gameObject.SetActive(false);
+        }
+    }
     public void InteractLoadScene() {
         if(GameState.currentProgressLevel >= level) {
             SceneManager.LoadScene(SceneName);
