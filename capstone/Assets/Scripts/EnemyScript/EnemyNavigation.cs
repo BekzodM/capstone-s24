712bc939df00b1d03c5191017294e548ba66727d
BekzodM <bmusayev07@gmail.com>
2024-04-13 13:57:35 -0400

    Enemy can follow player.

--- capstone/Assets/Scripts/EnemyScript/EnemyNavigation.cs
+++ capstone/Assets/Scripts/EnemyScript/EnemyNavigation.cs
@@ -7,23 +7,49 @@ public class EnemyNavigation : MonoBehaviour
 {
     Transform homeBase;
     private NavMeshAgent enemy;
+    public Transform player;
+    public LayerMask whatIsPlayer;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    void Awake()
+    {
+        player = GameObject.Find("Female 1").transform;
+        enemy = GetComponent<NavMeshAgent>();
+        whatIsPlayer = 1 << LayerMask.NameToLayer("Player");
+        sightRange = 10;
+        attackRange = 5;
+    }
+
     
     // Start is called before the first frame update
     void Start()
     {
-        enemy = GetComponent<NavMeshAgent>();
         homeBase = GameObject.Find("Base Controller").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.destination = homeBase.position;
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if(!playerInSightRange && !playerInAttackRange) enemy.SetDestination(homeBase.position);
+        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if(playerInSightRange && playerInAttackRange) AttackPlayer();
+        
     }
 
-    public void SetHomeBase(Transform newHomeBase)
+    void ChasePlayer()
     {
-        homeBase = newHomeBase;
-        enemy.destination = homeBase.position;
+        enemy.SetDestination(player.position);
+
+    }
+
+    void AttackPlayer()
+    {
+        enemy.SetDestination(transform.position);
+        transform.LookAt(player);
     }
 }
