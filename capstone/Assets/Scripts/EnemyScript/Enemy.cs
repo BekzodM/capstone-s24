8080603c3c9f9422b6ef7e327268fffd90a457f2
BekzodM <bmusayev07@gmail.com>
2024-04-13 18:57:34 -0400

    Enemy attack's player.

--- capstone/Assets/Scripts/EnemyScript/Enemy.cs
+++ capstone/Assets/Scripts/EnemyScript/Enemy.cs
@@ -8,34 +8,13 @@ public class Enemy : MonoBehaviour
     public int maxHealth = 100;
     int currentHealth;
     public HealthBar healthBar;
-
-    public Transform attackPoint;
-    float attackRange = 4f;
-    public string playerTag = "Player";
-    float attackCooldown = 1f;
-    float lastAttackTime = -9999f;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
     }
 
-    void OnTriggerStay(Collider other) {
-        if(other.CompareTag(playerTag)) {
-            Debug.Log("Player entered trigger zone");
-            if(Time.time - lastAttackTime >= attackCooldown) {
-                Debug.Log("Cooldown passed");
-                Vector3 closestPoint = other.ClosestPoint(attackPoint.position); 
-                float distance = Vector3.Distance(attackPoint.position, closestPoint);
-                Debug.Log("Distance to player: " + distance);
-                if (distance <= attackRange) {
-                    Debug.Log("Player within attack range, dealing damage");
-                    other.GetComponent<Player>().TakeDamage(10);
-                    lastAttackTime = Time.time;
-                }
-            }
-        }
-    }
+    
 
     /*
     Called by Player or Structure to deduct
@@ -62,12 +41,6 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
-
-    // Dev Function - visualize attack range.
-    void OnDrawGizmosSelected() {
-        if(attackPoint == null) return;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
 
