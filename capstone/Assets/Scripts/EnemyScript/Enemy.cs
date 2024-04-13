9eda418ea438a13280fb9de085f72abf367bf546
BekzodM <bmusayev07@gmail.com>
2024-04-13 18:44:38 -0400

    Edit enemey.

--- capstone/Assets/Scripts/EnemyScript/Enemy.cs
+++ capstone/Assets/Scripts/EnemyScript/Enemy.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -10,9 +10,9 @@ public class Enemy : MonoBehaviour
     public HealthBar healthBar;
 
     public Transform attackPoint;
-    public float attackRange = 0.5f;
+    float attackRange = 4f;
     public string playerTag = "Player";
-    float attackCooldown = 2f;
+    float attackCooldown = 1f;
     float lastAttackTime = -9999f;
     // Start is called before the first frame update
     void Start()
@@ -22,10 +22,14 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerStay(Collider other) {
         if(other.CompareTag(playerTag)) {
+            Debug.Log("Player entered trigger zone");
             if(Time.time - lastAttackTime >= attackCooldown) {
+                Debug.Log("Cooldown passed");
                 Vector3 closestPoint = other.ClosestPoint(attackPoint.position); 
                 float distance = Vector3.Distance(attackPoint.position, closestPoint);
+                Debug.Log("Distance to player: " + distance);
                 if (distance <= attackRange) {
+                    Debug.Log("Player within attack range, dealing damage");
                     other.GetComponent<Player>().TakeDamage(10);
                     lastAttackTime = Time.time;
                 }
@@ -54,7 +58,7 @@ public class Enemy : MonoBehaviour
         //remove killed enemy from enemiesInZone list
         attacker.GetComponent<Offensive>().RemoveEnemyFromZone(gameObject);
         
-        if(currentHealth == 0)
+        if(currentHealth <= 0)
         {
             Destroy(gameObject);
         }
