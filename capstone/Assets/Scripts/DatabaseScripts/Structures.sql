6998dfbf5748571db0144a482e6956d640440fc6
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2024-04-13 13:53:41 -0400

    created database init script
    database gets created on start of game

--- capstone/Assets/Scripts/DatabaseScripts/Structures.sql
+++ capstone/Assets/Scripts/DatabaseScripts/Structures.sql
@@ -11,6 +11,58 @@ INSERT INTO
         progress_level
     )
 VALUES
-    (1, "Turret", "Offensive","A basic shooting turret", "Sprites/PlanningPhase/turretIcon", 10, 100, 50, 1),
-    (2, "Wall", "Defensive", "A basic wall", "Sprites/PlanningPhase/wallIcon", 0, 200, 50, 1),
-    (3, "Idol", "Support", "The idol that heals the heart", "Sprites/PlanningPhase/turretIcon", 0, 100, 50, 1);
+    (
+        1,
+        "Turret",
+        "Offensive",
+        "A basic shooting turret",
+        "Sprites/PlanningPhase/turretIcon",
+        10,
+        100,
+        50,
+        1
+    ),
+    (
+        2,
+        "Wall",
+        "Defensive",
+        "A basic wall",
+        "Sprites/PlanningPhase/wallIcon",
+        0,
+        200,
+        50,
+        1
+    ),
+    (
+        3,
+        "Idol",
+        "Support",
+        "The idol that heals the heart",
+        "Sprites/PlanningPhase/turretIcon",
+        0,
+        100,
+        50,
+        1
+    ),
+    (
+        4,
+        "FlameThrower",
+        "Offensive",
+        "A basic flamethrower turret",
+        "Sprites/PlanningPhase/turretIcon",
+        2,
+        100,
+        50,
+        1
+    ),
+    (
+        5,
+        "FireTrap",
+        "Trap",
+        "A basic fire trap",
+        "Sprites/PlanningPhase/turretIcon",
+        2,
+        100,
+        50,
+        1
+    );
