ddd8c1250f87bd775933354e8542bb3fc7eb319d
BekzodM <bmusayev07@gmail.com>
2024-04-16 11:45:30 -0400

    Enemy attack without trigger colliders.

--- capstone/Assets/Scripts/EnemyScript/EnemyNavigation.cs
+++ capstone/Assets/Scripts/EnemyScript/EnemyNavigation.cs
@@ -9,7 +9,7 @@ public class EnemyNavigation : MonoBehaviour
     Transform homeBase;
     private NavMeshAgent enemy;
     public Transform player;
-    public Vector3 structure;
+    [System.NonSerialized] public Collider structure;
     public LayerMask whatIsStructure;
     public LayerMask whatIsPlayer;
 
@@ -17,11 +17,13 @@ public class EnemyNavigation : MonoBehaviour
     public bool playerInSightRange, playerInAttackRange;
     public bool structureInSightRange, structureInAttackRange;
 
+    public EnemyAttack enemyAttack;
 
     void Awake()
     {
         player = GameObject.Find("Female 1").transform;
         enemy = GetComponent<NavMeshAgent>();
+        enemyAttack = GetComponent<EnemyAttack>();
         whatIsPlayer = 1 << LayerMask.NameToLayer("Player");
         whatIsStructure = 1 << LayerMask.NameToLayer("Draggable");
         sightRange = 10;
@@ -38,18 +40,21 @@ public class EnemyNavigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks if a structure (Draggable Layered object) is in sight range
         structureInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsStructure);
+        // Checks if a structure (Draggable Layered object) is in attack range
         structureInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsStructure);
 
+        // Checks if player is in sight range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        // Checks if player is in attack range
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        //if(!playerInSightRange && !playerInAttackRange && !DestinationReachable()) enemy.SetDestination(homeBase.position);
-        if(playerInSightRange && !playerInAttackRange && PlayerReachable()) ChasePlayer();
-        else if(playerInSightRange && playerInAttackRange) AttackPlayer();
-        else if(structureInSightRange && !structureInAttackRange && StructureReachable()) ChaseStructure();
-        else if(structureInSightRange && structureInAttackRange) AttackStructure();
-        else enemy.SetDestination(homeBase.position);
+        if(playerInSightRange && !playerInAttackRange && PlayerReachable()) ChasePlayer();  // 1st Prioirty = Player: if nearby chase player
+        else if(playerInSightRange && playerInAttackRange) AttackPlayer();  // if within enemy attack range then deal damage to player
+        else if(structureInSightRange && !structureInAttackRange) ChaseStructure(); // 2nd Priority = Structure: if nearyby go to structure
+        else if(structureInSightRange && structureInAttackRange) AttackStructure(); // if within enemy attack range then deal damage to structure
+        else enemy.SetDestination(homeBase.position); // If nothing nearby, go to base
         
     }
 
@@ -60,28 +65,38 @@ public class EnemyNavigation : MonoBehaviour
     }
 
     void ChaseStructure() {
-        // Get the position of the structure
+        // Get all structure colliders nearby
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, sightRange, whatIsStructure);
         if (hitColliders.Length > 0)
         {
-            structure = hitColliders[0].transform.position;
-            // Store the position of the first detected structure
-            enemy.SetDestination(structure);
-            // You can also iterate through hitColliders to find the closest or any other criteria
-        }
+            structure = hitColliders[0]; // Store the position of the first detected structure
+            if(StructureReachable()) enemy.SetDestination(structure.transform.position); // If Structure reachable go to it
+            else enemy.SetDestination(homeBase.position); // Else go to base
+        } else enemy.SetDestination(homeBase.position); // If no structure colliders go to base
     }
 
+    /*
+    Make enemy stay in one place.
+    Deal damage to structure detected by ChaseStructure while looking at it.
+    */
     void AttackStructure() {
         enemy.ResetPath();
-        transform.LookAt(structure);
+        if(structure != null) {
+            enemyAttack.EnemyAttackStructure(structure);
+            transform.LookAt(structure.transform.position);
+        } else ChaseStructure();
     }
 
     void AttackPlayer()
     {
         enemy.ResetPath();
         transform.LookAt(player);
+        enemyAttack.EnemyAttackPlayer();
     }
 
+    /*
+    Check if Player is within NavMesh Area
+    */
     bool PlayerReachable(){
         NavMeshHit hit;
         bool isReachable = NavMesh.SamplePosition(player.position, out hit, 1f, NavMesh.AllAreas);
@@ -89,9 +104,12 @@ public class EnemyNavigation : MonoBehaviour
         return isReachable;
     }
 
+    /*
+    Check if Structure is within NavMesh Area
+    */
     bool StructureReachable(){
         NavMeshHit hit;
-        bool isReachable = NavMesh.SamplePosition(structure, out hit, 4f, NavMesh.AllAreas);
+        bool isReachable = NavMesh.SamplePosition(structure.transform.position, out hit, 2f, NavMesh.AllAreas);
 
         return isReachable;
     }
