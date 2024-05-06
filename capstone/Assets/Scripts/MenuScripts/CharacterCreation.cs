9b7213c1e4ffd3c11e130026271f360bf5e87a74
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2024-05-05 22:24:54 -0400

    added loading screen
    removed class option from menu

--- capstone/Assets/Scripts/MenuScripts/CharacterCreation.cs
+++ capstone/Assets/Scripts/MenuScripts/CharacterCreation.cs
@@ -6,21 +6,14 @@ using UnityEngine;
 public class CharacterCreation : MonoBehaviour
 {
     [SerializeField] TMP_InputField newName;
-    [SerializeField] TMP_Dropdown newClass;
 
     void Start()
     {
         GameState.playerName = "Mr. Player";
-        GameState.playerType = newClass.options[0].text;
     }
 
     public void setName()
     {
         GameState.playerName = newName.text;
-    }
-
-    public void setClass()
-    {
-        GameState.playerType = newClass.options[newClass.value].text;
     }
 }
